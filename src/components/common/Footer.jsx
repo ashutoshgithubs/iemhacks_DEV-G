3ce87718dcbe7ce7145752da828e7937e2bba083
Ashutosh Kumar <97340498+ashutoshgithubs@users.noreply.github.com>
2023-08-27 15:51:25 +0530

    Update Footer.jsx

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -172,7 +172,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made with ❤️ CodeHelp © 2023 Studynotion</div>
+          <div className="text-center">Made with ❤️ Bipul | AShutosh © 2023 DEV-G</div>
         </div>
       </div>
     </div>
