d06011898cd759c3d940792e23cb44332caf9b2a
Ashutosh Kumar <97340498+ashutoshgithubs@users.noreply.github.com>
2023-08-27 20:25:51 +0530

    last commit

--- src/components/core/Homepage/HighlightText.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HighLightText = ({text}) => {
-  return (
-    <span className='font-bold  text-richblue-200'>{" "}{text}</span>
-  )
-}
-
-export default HighLightText
