4940892f35947aef1a61f47e6fe6ba066022232d
Ashutosh Kumar <97340498+ashutoshgithubs@users.noreply.github.com>
2023-08-27 15:47:53 +0530

    Update Footer.jsx
    Footer enhanced

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -3,7 +3,7 @@ import { FooterLink2 } from "../../data/footer-links";
 import { Link } from "react-router-dom";
 
 // Images
-import Logo from "../../assets/Logo/Logo-Full-Light.png";
+import Logo from "../../assets/Logo/devlogo.png";
 
 // Icons
 import { FaFacebook, FaGoogle, FaTwitter, FaYoutube } from "react-icons/fa";
