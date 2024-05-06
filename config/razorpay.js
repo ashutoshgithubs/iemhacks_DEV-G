67ea3f3274b1cd452f87dcdc6ebf75dd8ab3a9c7
ashutoshgithubs <kumarashutosh1956@gmail.com>
2024-05-06 12:50:07 +0530

    initial commit

--- config/razorpay.js
+++
@@ -1,6 +0,0 @@
-const Razorpay=require('razorpay');
-require('dotenv').config()
-exports.instance=new Razorpay({
-    key_id:process.env.RAZORPAY_KEY,
-    key_secret:process.env.RAZORPAY_SECRET
-})
