94ea7dad89a24d6d73b14e4c50661bb6e7ebaf4b
karan singh D <ceitkaransingh2026@gmail.com>
2024-05-17 17:59:22 +0530

    added scroll counting effect

--- src/components/core/About/Stats.jsx
+++ src/components/core/About/Stats.jsx
@@ -1,27 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
+import CountUp from 'react-countup';
+import ScrollTrigger from 'react-scroll-trigger';
 const data = [
-  { count: "5K", label: "Active Students" },
-  { count: "10+", label: "Mentors" },
-  { count: "200+", label: "Courses" },
-  { count: "50+", label: "Awards" },
+  { count: "5000", label: "Active Students" },
+  { count: "10", label: "Mentors" },
+  { count: "200", label: "Courses" },
+  { count: "50", label: "Awards" },
 ];
 export default function Stats() {
+  const [count, setCount] = useState(false)
   return (
     <div className="flex flex-col gap-10 justify-between w-11/12 max-w-maxContent text-white mx-auto ">
+      
       <div className="grid grid-cols-2 md:grid-cols-4 text-center">
         {data.map((item, index) => {
           return (
+          <ScrollTrigger  onEnter={()=> setCount(true)} onExit={()=>setCount(false)}>
             <div className="flex flex-col py-10" key={index}>
               <h1 className="text-[30px] font-bold text-richblack-5">
-                {item.count}
+               {count && <CountUp start={0} end={item.count} duration={1}>
+                </CountUp>}+
               </h1>
               <h2 className="font-semibold text-[16px] text-richblack-500">
                 {item.label}
               </h2>
             </div>
+          </ScrollTrigger>
           );
         })}
       </div>
+      
     </div>
   );
 }
